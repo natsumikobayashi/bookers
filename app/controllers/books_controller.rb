938ea08a0fed3274b366d87dab2e9e722dcfc8a9
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-07 14:07:10 +0000

    投稿ボタン作成完了　次回indexに各リンクボタン作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,28 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books=Book.all
-    book=Book.new
-    book.save
+    @books=Book.all #booksをすべて表示
+    @book=Book.new #データを受け取り新規作成するための空箱
   end
 
   def edit
+    @book=Book.find(params[:id]) #データを取得
+  end
+  
+  def create
+    book =Book.new(book_params) #データを受け取り新規登録するための空箱
+    book.save #bookを保存
+    redirect_to '/books/:id' #ルーティングで投稿内容によって違うページが表示されるように
+  end
+  
+  def destroy
+    book=Book.find(params[:id]) #データを1件取得
+    book.destroy #データ削除
+    redirect_to '/books' #books一覧画面へリダイレクト
   end
   
   private
-  def list_params
-    require.permit(:title, :body)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'top'=>'homes#home'
-  get 'show'=>'books#show'
-  get 'lists'=>'books#index'
-  get 'edit'=>'books#edit'
-  get 'new'=>'books#new'
-  post 'lists'=>'books#show'
+  get 'books/:id'=>'books#show', as: 'book'
+  get 'books'=>'books#index'
+  get 'books/:id/edit'=>'books#edit', as: 'edit_book'
+  post 'books'=>'books#create'
+  delete 'books/:id'=> 'books#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
