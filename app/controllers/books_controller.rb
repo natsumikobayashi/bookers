801b2853d40396bb8b13472eb6cc1b099d03987a
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-08 14:27:18 +0000

    テストかけた　次回は修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   end
 
   def show
-   @book = Book.find(params[:id]) #見たいページを
+   @book = Book.find(params[:id]) #見たいページを取得
   end
 
   def index
@@ -15,23 +15,31 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id]) #編集したいデータを取得
   end
   
-  def create
-    book =Book.new(book_params) #データを受け取り新規登録するための空箱
-    book.save #bookを保存
-    redirect_to book_path(book.id) #ルーティングで投稿内容によって違うページが表示されるように
+  def create #viewに渡す必要があるため、インスタンス変数使う
+    @book =Book.new(book_params) #データを受け取り新規登録するための空箱
+    if @book.save #@bookを保存
+    flash[:notice] = "Book was successfully created." #投稿完了のフラッシュメッセージ表示
+    redirect_to book_path(@book.id) #ルーティングで投稿内容によって違うページが表示されるように
+    else
+      @books= Book.all #renderアクションのためにインスタンス変数(@books)を定義
+      render :index #indexページへ飛ぶように
+    end
   end
   
-  def destroy
+  def destroy #viewに渡す必要がないためローカル変数使う
     book=Book.find(params[:id]) #消すデータを取得
     book.destroy #データ削除
     redirect_to '/books' #books一覧画面へリダイレクト
   end
   
-  def update
+  def update #viewに渡す必要がないためローカル変数使う
     book = Book.find(params[:id]) #更新するデータ取得
     book.update(book_params) #データ更新
+      flash[:notice] = "Book was successfully updated." #更新できたフラッシュメッセージ表示
     redirect_to book_path(book.id) #更新した内容が分かるviewへリダイレクト
   end
+  
+  
   
   private
   def book_params
